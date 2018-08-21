e60025bb854e14f9c425bff180c7e08a73a2c15a
Alex Fiennes <alexfiennes246@gmail.com>
2018-08-21 12:31:32 +0100

    Categories added to item model

---
+++ app/models/add_column_to_item.rb
@@ -0,0 +1,2 @@
+class AddColumnToItem < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_20_155659) do
+ActiveRecord::Schema.define(version: 2018_08_21_112540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2018_08_20_155659) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "category"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
