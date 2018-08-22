c6e66843dc4ddf78693c28d396ae52bb29913bb1
Klaudiusz Zembrzuski <moa21@o2.pl>
2018-08-22 12:01:39 +0100

    Schema for Items column added available - defaulted to false '

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_22_105325) do
+ActiveRecord::Schema.define(version: 2018_08_22_105927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2018_08_22_105325) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "category"
+    t.boolean "available", default: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
