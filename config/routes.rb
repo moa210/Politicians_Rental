1c95231e3d1b1449c87ab534c20d048cdaff488b
Alex Fiennes <alexfiennes246@gmail.com>
2018-08-23 17:46:28 +0100

    random things

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # authentication
   devise_for :users
-  resources :bookings, except: [:new, :create]
+  resources :bookings, except: [:new, :create, :edit, :update]
   get 'user_items', to: 'items#user_items', as: 'user_items'
   resources :items do
-    resources :bookings, only: [:new, :create]
+    resources :bookings, only: [:new, :create, :edit, :update]
   end
   resources :users, only: [:show, :edit, :update]
   # get 'item/show'
