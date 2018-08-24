967bc6e3a3a4081e4e47e681dad7e9af5b7d6a0e
Nayela Wickramasuriya <nayelawick@gmail.com>
2018-08-24 13:06:05 +0100

    Landing page and other minor changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -41,55 +41,87 @@ User.destroy_all
 
 # Theresa May
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/theresa_may.jpg"
-user_1 = User.new(email: "TheresaMay-RealOne@gov.uk", full_name: "Theresa May", politician: true, password: "123123", about: nil)
+user_1 = User.new(email: "TheresaMay-RealOne@gov.uk", full_name: "Theresa May", politician: true, password: "123123", about: "Theresa Mary May is a British politician serving as Prime Minister of the United Kingdom and Leader of the Conservative Party since 2016. She served as Home Secretary from 2010 to 2016.")
 user_1.remote_photo_url = url
 user_1.save!
 
 # Boris Johnson
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021671/Politicians%20Rental/boris_johnson.jpg"
-user_2 = User.new(email: "BrexitBulldog@gov.uk", full_name: "Boris Johnson", politician: true, password: "123123", about: nil)
+user_2 = User.new(email: "BrexitBulldog@gov.uk", full_name: "Boris Johnson", politician: true, password: "123123", about: "Alexander Boris de Pfeffel Johnson, better known as Boris Johnson, is a British politician, popular historian, and journalist. He has been the Member of Parliament for Uxbridge and South Ruislip since 2015.")
 user_2.remote_photo_url = url
 user_2.save!
 
 # Ed Milliband
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021671/Politicians%20Rental/ed_milliband.jpg"
-user_3 = User.new(email: "EdyMilliband@gov.uk", full_name: "Ed Milliband", politician: true, password: "123123", about: nil)
+user_3 = User.new(email: "EdyMilliband@gov.uk", full_name: "Ed Milliband", politician: true, password: "123123", about: "Edward Samuel Miliband is a British politician who was Leader of the Labour Party as well as Leader of the Opposition between 2010 and 2015.")
 user_3.remote_photo_url = url
 user_3.save!
 
 # Jeremy Corbyn
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/jeremy_corbyn.jpg"
-user_4 = User.new(email: "ForTheMany@gov.uk", full_name: "Jeremy Corbyn", politician: true, password: "123123", about: nil)
+user_4 = User.new(email: "ForTheMany@gov.uk", full_name: "Jeremy Corbyn", politician: true, password: "123123", about: "Jeremy Bernard Corbyn is a British politician serving as Leader of the Labour Party and Leader of the Opposition since 2015.")
 user_4.remote_photo_url = url
 user_4.save!
 
 # Shami Chakrabarti
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/shami_chakrabarti.jpg"
-user_5 = User.new(email: "ChakrabatiDontExpel@gov.uk", full_name: "Shami Chakrabarti", politician: true, password: "123123", about: nil)
+user_5 = User.new(email: "ChakrabatiDontExpel@gov.uk", full_name: "Shami Chakrabarti", politician: true, password: "123123", about: "Sharmishta Chakrabarti, Baroness Chakrabarti, CBE, PC, commonly known as Shami Chakrabarti, is a British Labour Party politician and member of the House of Lords.")
 user_5.remote_photo_url = url
 user_5.save!
 
 # Nicola Sturgeon
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021670/Politicians%20Rental/nicola_sturgeon.jpg"
-user_6 = User.new(email: "MyReferendum@gov.uk", full_name: "Nicola Sturgeon", politician: true, password: "123123", about: nil)
+user_6 = User.new(email: "MyReferendum@gov.uk", full_name: "Nicola Sturgeon", politician: true, password: "123123", about: "Nicola Ferguson Sturgeon is a Scottish politician who is the current First Minister of Scotland and leader of the Scottish National Party, in office since November 2014. She is the first woman to hold either position.")
 user_6.remote_photo_url = url
 user_6.save!
 
 # Jeremy Hunt
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/jeremy_hunt.jpg"
-user_7 = User.new(email: "JeremyHunt-notDoH@gov.uk", full_name: "Jeremy Hunt", politician: true, password: "123123", about: nil)
+user_7 = User.new(email: "JeremyHunt-notDoH@gov.uk", full_name: "Jeremy Hunt", politician: true, password: "123123", about: "Jeremy Richard Streynsham Hunt is a British Conservative Party politician.")
 user_7.remote_photo_url = url
 user_7.save!
 
 
 ######### Items are below #########
 
-#### Theresa May's items
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021673/Politicians%20Rental/wheat_fields_TM.jpg"
 item = Item.new(description: "Wheat Fields", price: "15", category: "miscellaneous")
 item.user = user_1
 item.remote_photo_url = url
 item.save!
+
+url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/underwear.jpg"
+item = Item.new(description: "Wig", price: "15", category: "miscellaneous")
+item.user = user_2
+item.remote_photo_url = url
+item.save!
+
+url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/bacon_sandwich.jpg"
+item = Item.new(description: "Bacon Sandwich", price: "3", category: "miscellaneous")
+item.user = user_3
+item.remote_photo_url = url
+item.save!
+
+url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021670/Politicians%20Rental/nhs_JH.png"
+item = Item.new(description: "Save! NHS poster", price: "0", category: "miscellaneous")
+item.user = user_7
+item.remote_photo_url = url
+item.save!
+
+url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021671/Politicians%20Rental/referendum.jpg"
+item = Item.new(description: "Referendum", price: "90", category: "miscellaneous")
+item.user = user_6
+item.remote_photo_url = url
+item.save!
+
+url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021674/Politicians%20Rental/wreath_JC.jpg"
+item = Item.new(description: "Wreath", price: "8", category: "miscellaneous")
+item.user = user_4
+item.remote_photo_url = url
+item.save!
+
+
+####################### First 6 items for display on home page are above
 
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/fish_chips_TM.jpg"
 item = Item.new(description: "Fish and Chips", price: "5", category: "miscellaneous")
@@ -106,14 +138,6 @@ item.save!
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021673/Politicians%20Rental/car_all.jpg"
 item = Item.new(description: "Car", price: "35", category: "transport")
 item.user = user_1
-item.remote_photo_url = url
-item.save!
-
-
-#### Boris Johnson's items
-url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/underwear.jpg"
-item = Item.new(description: "Wig", price: "15", category: "miscellaneous")
-item.user = user_2
 item.remote_photo_url = url
 item.save!
 
@@ -136,26 +160,12 @@ item.remote_photo_url = url
 item.save!
 
 
-#### Ed Milliband's items
-url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/bacon_sandwich.jpg"
-item = Item.new(description: "Bacon Sandwich", price: "3", category: "miscellaneous")
-item.user = user_3
-item.remote_photo_url = url
-item.save!
-
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/house_all.jpg"
 item = Item.new(description: "House", price: "100", category: "accommodation")
 item.user = user_3
 item.remote_photo_url = url
 item.save!
 
-
-#### Jeremy Corbyn's items
-url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021674/Politicians%20Rental/wreath_JC.jpg"
-item = Item.new(description: "Wreath", price: "8", category: "miscellaneous")
-item.user = user_4
-item.remote_photo_url = url
-item.save!
 
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021671/Politicians%20Rental/bicycle_all.jpg"
 item = Item.new(description: "House", price: "75", category: "accommodation")
@@ -169,35 +179,19 @@ item.user = user_4
 item.remote_photo_url = url
 item.save!
 
-
-#### Shami Chakrabarti's items
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021671/Politicians%20Rental/peerage_SC.jpg"
 item = Item.new(description: "Life Peerage", price: "10", category: "miscellaneous")
 item.user = user_5
 item.remote_photo_url = url
 item.save!
 
-
-#### Nicola Sturgeon
 url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021672/Politicians%20Rental/house_all.jpg"
 item = Item.new(description: "House", price: "85", category: "accommodation")
 item.user = user_6
 item.remote_photo_url = url
 item.save!
 
-url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021671/Politicians%20Rental/referendum.jpg"
-item = Item.new(description: "Referendum", price: "90", category: "miscellaneous")
-item.user = user_6
-item.remote_photo_url = url
-item.save!
 
-
-#### Jeremy Hunt's items
-url = "https://res.cloudinary.com/dodobzdpn/image/upload/v1535021670/Politicians%20Rental/nhs_JH.png"
-item = Item.new(description: "Save! NHS poster", price: "0", category: "miscellaneous")
-item.user = user_6
-item.remote_photo_url = url
-item.save!
 
 # puts "I have created #{User.count} users and #{Item.count} items"
 
